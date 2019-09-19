024c981d5085f34c1008adf3211b66eba14c3bc7
asdfning <413125687@qq.com>
2019-09-19 09:28:07 +0800

    Initial commit

---
+++ src/main/java/com/pf/Vuetest2Application.java
@@ -0,0 +1,13 @@
+package com.pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vuetest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vuetest2Application.class, args);
+    }
+
+}
